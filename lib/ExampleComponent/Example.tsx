7552967ec6e2f7bfb3be757cbcd39609aacc2d3b
ponytakxn <ju.valdesv@duocuc.cl>
2024-04-22 18:41:07 -0400

    @ponytakxn config lint

--- lib/ExampleComponent/Example.tsx
+++ lib/ExampleComponent/Example.tsx
@@ -3,25 +3,25 @@ import { cva, VariantProps } from 'class-variance-authority'
 import { cn } from '../utils/index'
 
 const exampleStyles = cva([
-  'font-semibold',
-  'disabled:cursor-not-allowed',
-  'disabled:bg-primary/50',
-  'disabled:text-white/50',
-  'transition-colors',
-  'duration-300',
-  'ease-in-out',
+  "font-semibold",
+  "disabled:cursor-not-allowed",
+  "disabled:bg-primary/50",
+  "disabled:text-white/50",
+  "transition-colors",
+  "duration-300",
+  "ease-in-out"
 ],
   {
     variants: {
       size: {
-        sm: 'text-base py-xs px-md',
-        md: 'text-md py-sm px-lg',
-        lg: 'text-lg py-md px-xl',
+        sm: "text-base py-xs px-md",
+        md: "text-md py-sm px-lg",
+        lg: "text-lg py-md px-xl",
       },
       variant: {
-        solid: 'bg-primary text-white',
-        outline: 'bg-transparent border border-primary text-primary hover:bg-primary hover:text-white',
-        ghost: 'bg-transparent text-primary hover:bg-primary hover:text-white'
+        solid: "bg-primary text-white",
+        outline: "bg-transparent border border-primary text-primary hover:bg-primary hover:text-white",
+        ghost: "bg-transparent text-primary hover:bg-primary hover:text-white"
       }
     }
   })
