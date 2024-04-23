d982b1cc4db882525193531ee97c54845142036b
sebathemaker <106289007+sebathemaker@users.noreply.github.com>
2024-04-22 21:57:13 -0400

    Basic/checkbox (#3)
    * @sebathemaker creacion de checkbox
    
    * @sebathemaker creacion de checkbox
    
    * @sebathemaker creacion checkbox

---
+++ lib/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Checkbox } from './Checkbox';
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+  title: 'Checkbox',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Basic: Story = {
+  args: {
+    color: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    color: 'secondary',
+  },
+};
+
+export const Danger: Story = {
+  args: {
+    color: 'danger',
+  },
+};
+
+export const Colors: Story = {
+  render: () => (
+    <div className='grid gap-8'>
+      <Checkbox color='primary' />
+      <Checkbox color='secondary' />
+      <Checkbox color='danger' />
+      <Checkbox color='success' />
+      <Checkbox color='info' />
+      <Checkbox color='warning' />
+    </div>
+  ),
+};
+
+export const Disabled: Story = {
+  render: () => (
+    <Checkbox color='primary' disabled />
+  ),
+};
---
+++ lib/Checkbox/Checkbox.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { forwardRef, type ComponentProps } from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+import { cn } from '../utils/index'
+
+const checkboxStyles = cva(['border', 'border-primary'], {
+  variants: {
+    color: {
+      primary: 'accent-primary',
+      secondary: 'accent-secondary',
+      danger: 'accent-danger',
+      success: 'accent-success',
+      info: 'accent-info',
+      warning: 'accent-warning',
+    },
+  },
+})
+
+type CheckboxProps = ComponentProps<'input'> &
+  VariantProps<typeof checkboxStyles>
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, color, ...props }, ref) => {
+    return (
+      <input
+        type='checkbox'
+        ref={ref}
+        className={cn(checkboxStyles({ color, className }))}
+        {...props}
+      />
+    )
+  }
+)
+
+export default Checkbox
