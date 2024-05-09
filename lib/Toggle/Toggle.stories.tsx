facc052af004fae760defd35a6a3906d3f81e853
pony-v <juan.valdes@applydigital.com>
2024-05-09 17:21:31 -0400

    toggle terminado

--- lib/Toggle/Toggle.stories.tsx
+++ lib/Toggle/Toggle.stories.tsx
@@ -4,10 +4,6 @@ import { Toggle } from './Toggle'
 const meta: Meta<typeof Toggle> = {
   component: Toggle,
   title: 'Toggle',
-  argTypes: {
-    onPressedChange: { action: 'pressed changed' },
-    onClick: { action: 'clicked' },
-  },
 }
 
 export default meta
@@ -16,17 +12,6 @@ type Story = StoryObj<typeof Toggle>
 
 export const Default: Story = {
   args: {
-    defaultPressed: false,
-  },
-}
-export const Pressed: Story = {
-  args: {
-    defaultPressed: true,
-  },
-}
-export const Disabled: Story = {
-  args: {
-    defaultPressed: false,
-    disabled: true,
+    children: 'B',
   },
 }
--- lib/Toggle/Toggle.tsx
+++ lib/Toggle/Toggle.tsx
@@ -1,64 +1,29 @@
-import React, { forwardRef, useState, useEffect } from 'react'
+import React, { forwardRef, useState, ComponentProps } from 'react'
 import { cva } from 'class-variance-authority'
 import { cn } from '../utils/index'
 
-const ToggleStyles = cva({
-  backgroundColor: 'info',
-  border: '2px solid blue',
-  padding: '10px',
-  color: 'white',
-})
+const toggleStyles = cva([
+  'hover:bg-primary/10 cursor-pointer rounded-sm px-xs py-xxxs',
+])
 
-type ToggleProps = {
-  pressed?: boolean
-  defaultPressed?: boolean
-  onPressedChange?: (pressed: boolean) => void
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-  disabled?: boolean
-} & React.ButtonHTMLAttributes<HTMLButtonElement>
+type ToggleProps = ComponentProps<'button'>
 
 export const Toggle = forwardRef<HTMLButtonElement, ToggleProps>(
-  (
-    {
-      pressed: pressedProp,
-      defaultPressed = false,
-      onPressedChange,
-      onClick,
-      disabled,
-      ...props
-    },
-    ref
-  ) => {
-    const [pressed, setPressed] = useState(defaultPressed)
+  ({ className, children, ...props }, ref) => {
+    const [pressed, setPressed] = useState(false)
 
-    useEffect(() => {
-      if (pressedProp !== undefined) {
-        setPressed(pressedProp)
-      }
-    }, [pressedProp])
-
-    const handleClick = () => {
-      const newPressed = !pressed
-      setPressed(newPressed)
-      onPressedChange?.(newPressed)
-    }
+    const handleClick = () => setPressed(!pressed)
 
     return (
       <button
-        type='button'
-        aria-pressed={pressed}
-        disabled={disabled}
-        {...props}
         ref={ref}
-        onClick={(event) => {
-          if (!disabled) {
-            handleClick()
-          }
-          onClick?.(event)
-        }}
-        className={cn(ToggleStyles())}
+        className={cn(toggleStyles({ className }), {
+          'bg-secondary font-semibold hover:bg-secondary': pressed,
+        })}
+        onClick={handleClick}
+        {...props}
       >
-        B
+        {children}
       </button>
     )
   }
