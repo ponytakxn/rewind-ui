b99acd48173c85eb24e4143c7031b8de19b015df
ponytakxn <ju.valdesv@duocuc.cl>
2024-05-31 01:21:09 -0400

    @ponytakxn feat: Pagination created, HoverCard, Toggle and Pagination added to lib

---
+++ lib/Pagination/Pagination.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Pagination } from './Pagination'
+
+const meta: Meta<typeof Pagination> = {
+  component: Pagination,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Pagination>
+
+export const Default: Story = {
+  args: {
+    totalPages: 7,
+    page: 4,
+  },
+}
+
+export const First: Story = {
+  args: {
+    totalPages: 2,
+    page: 1,
+  },
+}
+
+export const Last: Story = {
+  args: {
+    totalPages: 6,
+    page: 6,
+  },
+}
---
+++ lib/Pagination/Pagination.tsx
@@ -0,0 +1,139 @@
+import { cva } from 'class-variance-authority'
+import { ComponentProps, forwardRef } from 'react'
+import { cn } from '../utils'
+
+const paginationStyles = cva([])
+
+const pageItemStyles =
+  'bg-transparent border-none h-8 w-8 mx-1 rounded-[50%] font-bold cursor-pointer text-primary hover:underline focus:outline-0'
+
+const sidesStyles =
+  'shadow-[transparent_0_0_0_1px_transparent_0_0_0_4px_rgba(0,0,0,0.18)_0_2px_4px] hover:no-decoration hover:shadow-[transparent_0_0_0_1px_transparent_0_0_0_4px_rgba(0,0,0,0.12)_0_6px_16px]'
+
+const activeStyles = '!bg-primary !text-secondary'
+
+const separatorStyles = 'w-4 mx-1'
+
+export interface Props {
+  page: number
+  totalPages: number
+  handlePagination: (page: number) => void
+  slug: string
+}
+
+type PaginationProps = ComponentProps<'div'> & Props
+
+export const Pagination = forwardRef<HTMLDivElement, PaginationProps>(
+  ({ page, totalPages, handlePagination, slug, className, ...props }, ref) => {
+    return (
+      <div ref={ref} className={cn(paginationStyles({ className }))} {...props}>
+        <div className='py-8 flex justify-center'>
+          {page !== 1 && (
+            <a href={`/${slug}/${page - 1}`}>
+              <button
+                onClick={() => handlePagination(page - 1)}
+                className={`${pageItemStyles} ${sidesStyles}`}
+              >
+                &lt;
+              </button>
+            </a>
+          )}
+
+          <a href={`/${slug}/1`}>
+            <button
+              onClick={() => handlePagination(1)}
+              className={
+                page === 1
+                  ? `${pageItemStyles} ${activeStyles}`
+                  : pageItemStyles
+              }
+            >
+              {1}
+            </button>
+          </a>
+
+          {page > 3 && <div className={separatorStyles}>...</div>}
+
+          {page === totalPages && totalPages > 3 && (
+            <a href={`/${slug}/${page - 2}`}>
+              <button
+                onClick={() => handlePagination(page - 2)}
+                className={pageItemStyles}
+              >
+                {page - 2}
+              </button>
+            </a>
+          )}
+
+          {page > 2 && (
+            <a href={`/${slug}/${page - 1}`}>
+              <button
+                onClick={() => handlePagination(page - 1)}
+                className={pageItemStyles}
+              >
+                {page - 1}
+              </button>
+            </a>
+          )}
+
+          {page !== 1 && page !== totalPages && (
+            <button
+              onClick={() => handlePagination(page)}
+              className={`${pageItemStyles} ${activeStyles}`}
+            >
+              {page}
+            </button>
+          )}
+
+          {page < totalPages - 1 && (
+            <a href={`/${slug}/${page + 1}`}>
+              <button
+                onClick={() => handlePagination(page + 1)}
+                className={pageItemStyles}
+              >
+                {page + 1}
+              </button>
+            </a>
+          )}
+
+          {page === 1 && totalPages > 3 && (
+            <a href={`/${slug}/${page + 2}`}>
+              <button
+                onClick={() => handlePagination(page + 2)}
+                className={pageItemStyles}
+              >
+                {page + 2}
+              </button>
+            </a>
+          )}
+
+          {page < totalPages - 2 && <div className={separatorStyles}>...</div>}
+
+          <a href={`/${slug}/${totalPages}`}>
+            <button
+              onClick={() => handlePagination(totalPages)}
+              className={
+                page === totalPages
+                  ? `${pageItemStyles} ${activeStyles}`
+                  : pageItemStyles
+              }
+            >
+              {totalPages}
+            </button>
+          </a>
+
+          {page !== totalPages && (
+            <a href={`/${slug}/${page + 1}`}>
+              <button
+                onClick={() => handlePagination(page + 1)}
+                className={`${pageItemStyles} ${sidesStyles}`}
+              >
+                &gt;
+              </button>
+            </a>
+          )}
+        </div>
+      </div>
+    )
+  }
+)
