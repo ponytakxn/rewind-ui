21a14d2cebb3617c2e9f4bc20799bc153bd57614
ponytakxn <ju.valdesv@duocuc.cl>
2024-06-06 19:23:06 -0400

    @ponytakxn feat: toast component added

---
+++ lib/Toast/ToastContent.tsx
@@ -0,0 +1,44 @@
+import { ComponentProps, forwardRef, useContext } from 'react'
+import { ToastContext } from './main'
+import { cva } from 'class-variance-authority'
+import { cn } from '../utils'
+import { createPortal } from 'react-dom'
+
+const toastContentStyles = cva([
+  'fixed bottom-4 right-4 bg-white text-primary border border-primary rounded-md w-[300px] z-20 p-sm',
+])
+
+type ToastContentProps = ComponentProps<'div'>
+
+export const ToastContent = forwardRef<HTMLDivElement, ToastContentProps>(
+  ({ children, className, ...props }, ref) => {
+    const { showToast, setShowToast } = useContext(ToastContext)
+
+    return (
+      <>
+        {showToast &&
+          createPortal(
+            <div
+              ref={ref}
+              className={cn(toastContentStyles({ className }))}
+              {...props}
+            >
+              {children}
+              <div
+                className='
+                absolute -top-2 -right-2 
+                bg-white border border-primary rounded-full w-lg h-lg 
+                flex items-center justify-center 
+                text-sm font-semibold cursor-pointer
+              '
+                onClick={() => setShowToast(false)}
+              >
+                X
+              </div>
+            </div>,
+            document.body
+          )}
+      </>
+    )
+  }
+)
